dff81dee5f876da9bd57e4dfb73cfffaaf999bdc
alicantarim <alicantarim@hotmail.com>
2024-11-04 12:46:25 +0300

    creates add profile.

--- src/App.jsx
+++ src/App.jsx
@@ -33,30 +33,54 @@ const APIDataStatic = [
 ];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState(APIDataStatic);
+  const [form, setForm] = useState({ username: "" });
+
+  const handleAddUser = (newUser) => {
+    setUsers([newUser, ...users]);
+  };
+
+  const handleChange = (event) => {
+    console.log(event);
+    const newUser = {
+      ...form,
+      [event.target.name]: event.target.value,
+    };
+
+    setForm(newUser);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const submittedUser = {
+      name: form.username,
+      id: Date.now(),
+      avatarUrl: `https://api.multiavatar.com/${form.username}.svg`,
+    };
+    setForm({ username: "" });
+    handleAddUser(submittedUser);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit="">
+        {form.username}
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="username"
+            id="username"
+            value={form.username}
+            placeholder="Kullanıcı adınızı giriniz."
+          />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+      {users.map((user) => (
+        <Profile key={user.id} info={user} />
+      ))}
     </>
   );
 }
